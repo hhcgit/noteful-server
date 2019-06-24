bf358734da84e19bc4ac7666a94ffe0aa3122fdc
Harrison Chen <harrisonchen@harrisons-mbp.lan>
2019-06-24 14:44:59 -0700

    a

--- src/server.js
+++ src/server.js
@@ -1,9 +1,9 @@
 const app = require('./app')
 const knex = require('knex')
 const { PORT, DATABASE_URL } = require('./config')
-const pg = require('pg');
 
-pg.defaults.ssl = true;
+
+
 const db = knex({
   client: 'pg',
   connection: DATABASE_URL,
