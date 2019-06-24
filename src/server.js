e5e834c3fd2e08c0325912833bc8bdf74edad272
Harrison Chen <harrisonchen@harrisons-mbp.lan>
2019-06-24 14:30:23 -0700

    added ssl

--- src/server.js
+++ src/server.js
@@ -1,7 +1,9 @@
 const app = require('./app')
 const knex = require('knex')
 const { PORT, DATABASE_URL } = require('./config')
+var pg = require('pg');
 
+pg.defaults.ssl = true;
 const db = knex({
   client: 'pg',
   connection: DATABASE_URL,
